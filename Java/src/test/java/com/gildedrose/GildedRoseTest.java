3c6ee19feaca896805e7aa0d44493ae7d4371b3d
leusj <jorgi.leus@brusselsairport.be>
2023-10-03 20:07:59 +0200

    Extend initial test to check for all name, sellIn and quality

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void itemNameRemainsTheSame() {
+    void normalItemUpdateDoesNotDecreaseQualityBelowZero() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
     }
 
 }
