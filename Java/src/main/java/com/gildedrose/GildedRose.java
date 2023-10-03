ba6c32ddb9e012b27b58b26b6b4199d97b4206d3
leusj <jorgi.leus@brusselsairport.be>
2023-10-03 20:40:14 +0200

    Simplify setting quality to zero when sellIn is below zero and item is a backstage pass and use simplified for loop

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,52 +10,52 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(AGED_BRIE.getName())
-                    && !items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(SULFURAS.getName())) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (!item.name.equals(AGED_BRIE.getName())
+                    && !item.name.equals(BACKSTAGE_PASSES.getName())) {
+                if (item.quality > 0) {
+                    if (!item.name.equals(SULFURAS.getName())) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.name.equals(BACKSTAGE_PASSES.getName())) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals(SULFURAS.getName())) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.name.equals(SULFURAS.getName())) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(AGED_BRIE.getName())) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES.getName())) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(SULFURAS.getName())) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals(AGED_BRIE.getName())) {
+                    if (!item.name.equals(BACKSTAGE_PASSES.getName())) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals(SULFURAS.getName())) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                      item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
