7506e915013eba443ba1739bb92322a7d55fe12f
leusj <jorgi.leus@brusselsairport.be>
2023-10-06 06:59:16 +0200

    Remove unused imports due to Factory taking over this responsibility... silly me

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,19 +7,9 @@ import static com.gildedrose.ItemCategory.fromName;
 class GildedRose {
 
     Item[] items;
-    private final NormalItemQualityUpdate normalItemQualityUpdate;
-    private final SulfarasItemQualityUpdate sulfarasItemQualityUpdate;
-    private final BackstagePassesItemQualityUpdate backstagePassesItemQualityUpdate;
-    private final AgedBrieItemQualityUpdate agedBrieItemQualityUpdate;
-    private final ConjuredItemQualityUpdate conjuredItemQualityUpdate;
 
     public GildedRose(Item[] items) {
         this.items = items;
-        this.normalItemQualityUpdate = new NormalItemQualityUpdate();
-        this.sulfarasItemQualityUpdate = new SulfarasItemQualityUpdate();
-        this.backstagePassesItemQualityUpdate = new BackstagePassesItemQualityUpdate();
-        this.agedBrieItemQualityUpdate = new AgedBrieItemQualityUpdate();
-        this.conjuredItemQualityUpdate = new ConjuredItemQualityUpdate();
     }
 
     public void updateQuality() {
